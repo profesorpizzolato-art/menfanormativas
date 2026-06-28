990470c06083dd196ec1a1a3abf6e3d2f11fc770
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-06-28 18:32:56 -0300

    Create normativa.js model for regulatory data

---
+++ modelos/normativas.js
@@ -0,0 +1,14 @@
+// modelos/normativa.js
+
+const normativa = {
+    id: Number,
+    titulo: String,
+    organismo: String,
+    categoria: String,
+    descripcion: String,
+    aplicacion_operativa: String,
+    caso_practico: String,
+    fecha_actualizacion: Date
+};
+
+module.exports = normativa;
