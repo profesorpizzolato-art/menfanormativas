224f6448413d490c098beb8385045e9f95bc9569
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-06-28 19:50:06 -0300

    Add obtener_normativa_por_id function
    Added function to retrieve normativa by ID and updated SQL queries.

--- modelos/normativa.py
+++ modelos/normativa.py
@@ -6,11 +6,11 @@ from base_datos.conexion import obtener_conexion
 def obtener_normativas():
 
     conexion = obtener_conexion()
-
     cursor = conexion.cursor()
 
     cursor.execute("""
         SELECT * FROM normativas
+        ORDER BY codigo
     """)
 
     datos = cursor.fetchall()
@@ -18,6 +18,23 @@ def obtener_normativas():
     conexion.close()
 
     return datos
+
+
+def obtener_normativa_por_id(id):
+
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        SELECT * FROM normativas
+        WHERE id = ?
+    """, (id,))
+
+    dato = cursor.fetchone()
+
+    conexion.close()
+
+    return dato
 
 
 def agregar_normativa(
@@ -31,7 +48,6 @@ def agregar_normativa(
         fecha_actualizacion):
 
     conexion = obtener_conexion()
-
     cursor = conexion.cursor()
 
     cursor.execute("""
@@ -54,3 +70,64 @@ def agregar_normativa(
         objetivo,
         alcance,
         aplicacion,
+        fecha_actualizacion
+    ))
+
+    conexion.commit()
+    conexion.close()
+
+
+def actualizar_normativa(
+        id,
+        codigo,
+        titulo,
+        organismo,
+        categoria,
+        objetivo,
+        alcance,
+        aplicacion,
+        fecha_actualizacion):
+
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        UPDATE normativas
+        SET
+            codigo = ?,
+            titulo = ?,
+            organismo = ?,
+            categoria = ?,
+            objetivo = ?,
+            alcance = ?,
+            aplicacion = ?,
+            fecha_actualizacion = ?
+        WHERE id = ?
+    """, (
+        codigo,
+        titulo,
+        organismo,
+        categoria,
+        objetivo,
+        alcance,
+        aplicacion,
+        fecha_actualizacion,
+        id
+    ))
+
+    conexion.commit()
+    conexion.close()
+
+
+def eliminar_normativa(id):
+
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+        DELETE FROM normativas
+        WHERE id = ?
+    """, (id,))
+
+    conexion.commit()
+    conexion.close()
