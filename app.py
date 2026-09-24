1e2c70e31f876d2719504911911f296d11445dd7
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-09-24 20:27:38 -0300

    Initialize database connection in app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import streamlit as st
 
+from base_datos.conexion import inicializar_base_datos
+
 from paginas import inicio
 from paginas import biblioteca
 from paginas import favoritos
 from paginas import perfil
 from paginas import administracion
+
+
+inicializar_base_datos()
 
 
 st.set_page_config(
